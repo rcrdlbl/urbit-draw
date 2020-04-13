96450dc4d9e215ac33a7f60ef7a3c3aa8d0314d5
rcrdlbl <bluh@outlook.com>
2020-04-12 23:20:58 -0400

    naming convention

---
+++ dist/js/components/sidebar.js
@@ -0,0 +1,21 @@
+const _jsxFileName = "/Users/przemyslaw/urbit-draw/src/js/components/sidebar.js";import React from "react";
+
+const Sidebar = () => {
+  return (
+    React.createElement('div', {
+      className: "bn br-m br-l br-xl b--gray4 b--gray1-d lh-copy h-100 flex-shrink-0 mw5-m mw5-l mw5-xl pt3 pt0-m pt0-l pt0-xl relative dn-s flex-basis-100-s flex-basis-30-ns"
+
+        , __self: this, __source: {fileName: _jsxFileName, lineNumber: 5}}
+    
+      , React.createElement('div', { className: "overflow-y-scroll h-100" , __self: this, __source: {fileName: _jsxFileName, lineNumber: 10}}
+        , React.createElement('div', { className: "w-100 bg-transparent" , __self: this, __source: {fileName: _jsxFileName, lineNumber: 11}}
+          , React.createElement('a', { href: "/~draw/new", className: "dib f9 pointer green2 gray4-d pa4"     , __self: this, __source: {fileName: _jsxFileName, lineNumber: 12}}, "New Canvas"
+
+          )
+        )
+      )
+    )
+  );
+};
+
+export default Sidebar;
